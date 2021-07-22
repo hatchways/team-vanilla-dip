48121be1708bb122761f012dec89beaeec30f4a3
LZhizhou <luzhizhou@hotmail.com>
2021-07-22 13:09:22 -0600

    upload image ui done

--- client/src/pages/Submit/Submit.tsx
+++ client/src/pages/Submit/Submit.tsx
@@ -1,15 +1,15 @@
-import useStyles from './useStyles';
 import { useAuth } from '../../context/useAuthContext';
 import { useSocket } from '../../context/useSocketContext';
 import React, { useEffect, useState } from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { Box, Button, Typography } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import Navbar from '../../components/Navbar/Navbar';
 import './Submit.css';
+import { uploadImage } from '../../helpers/APICalls/uploadImage';
+import { useHistory } from 'react-router-dom';
 export default function Submit(): JSX.Element {
-  const classes = useStyles();
   const { loggedInUser } = useAuth();
   const { initSocket } = useSocket();
   useEffect(() => {
@@ -31,148 +31,67 @@ export default function Submit(): JSX.Element {
   );
 }
 
-class FileUploader extends React.Component {
-  static counter = 0;
-  fileUploaderInput: HTMLElement | null = null;
+function FileUploader(): JSX.Element {
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-  state = { dragging: false, file: null };
-
-  dragEventCounter = 0;
-  dragenterListener = (event: React.DragEvent<HTMLDivElement>) => {
-    this.overrideEventDefaults(event);
-    this.dragEventCounter++;
-    if (event.dataTransfer.items && event.dataTransfer.items[0]) {
-      this.setState({ dragging: true });
-    } else if (event.dataTransfer.types && event.dataTransfer.types[0] === 'Files') {
-      // This block handles support for IE - if you're not worried about
-      // that, you can omit this
-      this.setState({ dragging: true });
-    }
-  };
-
-  dragleaveListener = (event: React.DragEvent<HTMLDivElement>) => {
-    this.overrideEventDefaults(event);
-    this.dragEventCounter--;
-
-    if (this.dragEventCounter === 0) {
-      this.setState({ dragging: false });
-    }
-  };
-
-  dropListener = (event: React.DragEvent<HTMLDivElement>) => {
-    this.overrideEventDefaults(event);
-    this.dragEventCounter = 0;
-    this.setState({ dragging: false });
-
-    if (event.dataTransfer.files && event.dataTransfer.files[0]) {
-      this.setState({ file: event.dataTransfer.files[0] });
-    }
-  };
-
-  overrideEventDefaults = (event: Event | React.DragEvent<HTMLDivElement>) => {
-    event.preventDefault();
-    event.stopPropagation();
-  };
-
-  onSelectFileClick = () => {
-    this.fileUploaderInput && this.fileUploaderInput.click();
-  };
-
-  onFileChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+  // On file select (from the pop up)
+  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    // Update the state
     if (event.target.files && event.target.files[0]) {
-      this.setState({ file: event.target.files[0] });
+      setSelectedFile(event.target.files[0]);
+    }
+  };
+  const history = useHistory();
+
+  // On file upload (click the upload button)
+  const onFileUpload = () => {
+    if (selectedFile) {
+      // Create an object of formData
+      const formData = new FormData();
+
+      // Update the formData object
+      formData.append('myFile', selectedFile, selectedFile.name);
+
+      // Details of the uploaded file
+      console.log(selectedFile);
+      uploadImage({ image: selectedFile }).then((r) => {
+        console.log(r);
+        history.goBack();
+      });
+      // Request made to the backend api
+      // Send formData object
     }
   };
 
-  componentDidMount() {
-    window.addEventListener('dragover', (event: Event) => {
-      this.overrideEventDefaults(event);
-    });
-    window.addEventListener('drop', (event: Event) => {
-      this.overrideEventDefaults(event);
-    });
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('dragover', this.overrideEventDefaults);
-    window.removeEventListener('drop', this.overrideEventDefaults);
-  }
-
-  render() {
-    return (
-      <FileUploaderPresentationalComponent
-        dragging={this.state.dragging}
-        file={this.state.file}
-        onSelectFileClick={this.onSelectFileClick}
-        onDrag={this.overrideEventDefaults}
-        onDragStart={this.overrideEventDefaults}
-        onDragEnd={this.overrideEventDefaults}
-        onDragOver={this.overrideEventDefaults}
-        onDragEnter={this.dragenterListener}
-        onDragLeave={this.dragleaveListener}
-        onDrop={this.dropListener}
-      >
-        <input
-          ref={(el) => (this.fileUploaderInput = el)}
-          type="file"
-          className="file-uploader__input"
-          onChange={this.onFileChanged}
-        />
-      </FileUploaderPresentationalComponent>
-    );
-  }
-}
-type PresentationalProps = {
-  dragging: boolean;
-  file: File | null;
-  onSelectFileClick: () => void;
-  onDrag: (event: React.DragEvent<HTMLDivElement>) => void;
-  onDragStart: (event: React.DragEvent<HTMLDivElement>) => void;
-  onDragEnd: (event: React.DragEvent<HTMLDivElement>) => void;
-  onDragOver: (event: React.DragEvent<HTMLDivElement>) => void;
-  onDragEnter: (event: React.DragEvent<HTMLDivElement>) => void;
-  onDragLeave: (event: React.DragEvent<HTMLDivElement>) => void;
-  onDrop: (event: React.DragEvent<HTMLDivElement>) => void;
-};
-
-function FileUploaderPresentationalComponent({
-  dragging,
-  file,
-  onSelectFileClick,
-  onDrag,
-  onDragStart,
-  onDragEnd,
-  onDragOver,
-  onDragEnter,
-  onDragLeave,
-  onDrop,
-  children,
-}: React.PropsWithChildren<PresentationalProps>): JSX.Element {
-  let uploaderClasses = 'file-uploader';
-  if (dragging) {
-    uploaderClasses += ' file-uploader--dragging';
-  }
-
-  const fileName = file ? file.name : 'No File Uploaded!';
+  // File content to be displayed after
+  // file upload is complete
+  const fileData = () => {
+    if (selectedFile) {
+      return (
+        <div>
+          <h2>File Details:</h2>
+          <p>File Name: {selectedFile.name}</p>
+          <p>File Type: {selectedFile.type}</p>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose before Pressing the Upload button</h4>
+        </div>
+      );
+    }
+  };
 
   return (
-    <div
-      className={uploaderClasses}
-      onDrag={onDrag}
-      onDragStart={onDragStart}
-      onDragEnd={onDragEnd}
-      onDragOver={onDragOver}
-      onDragEnter={onDragEnter}
-      onDragLeave={onDragLeave}
-      onDrop={onDrop}
-    >
-      <div className="file-uploader__contents">
-        <span className="file-uploader__file-name">{fileName}</span>
-        <span>Drag & Drop File</span>
-        <span>or</span>
-        <span onClick={onSelectFileClick}>Select File</span>
+    <div>
+      <h1>Upload Image</h1>
+      <div>
+        <input type="file" accept="image/*" onChange={onFileChange} />
+        <button onClick={onFileUpload}>Upload!</button>
       </div>
-      {children}
+      {fileData()}
     </div>
   );
 }
