32561fec327e914b6f1e469048dfa883f382888a
daniel <holuwadanzy@gmail.com>
2021-08-06 01:33:37 +0100

    Removed Consoles from gmail.js

--- server/controllers/gmail.js
+++ server/controllers/gmail.js
@@ -1,4 +1,6 @@
 const nodemailer = require("nodemailer");
+const User = require('../models/User');
+const Contest = require('../models/Contest');
 const asyncHandler = require("express-async-handler")
 
 // create transporter object with smtp server details
@@ -13,37 +15,40 @@ const transporter = nodemailer.createTransport({
 
 
 exports.sendWinnerEmail =  asyncHandler(async (req, res)=> {
-    const contestID = req.params.id;
-    const { receiverID } = req.body;
-    const receiver = await User.findById(receiverID);
-    
-    if (!receiver){
-        return res.status(404).json({
-            status: "Receiver not found"
-        })
-    }
-    const contest = await Contest.findById(contestID);
-    if (!contest){
-        return res.status(404).json({   
-            status: "Contest not found"
-        })
-    }
-    const message = {
-        from: 'TeamVanilla <teamvanilladip@gmail.com>',
-        to: receiver.email,
-        subject: "Contest Winner",
-        html: `<h1>You have been selected a winner in the '${contest.title}'</h1>`
-    }
-    transporter.sendMail(message, (err, info) => {
-        if (err) {
-            return res.status(500).json({status: "error",
-                err
-            })
-        } else {
-            return res.status(200).json({
-                status: 'Email Sent Successfully',
-                info
+    try {
+        const contestID = req.params.id;
+        const { receiverID } = req.body;
+        const receiver = await User.findById(receiverID);
+        if (!receiver){
+            return res.status(404).json({
+                status: "Receiver not found"
             })
         }
-    })
+        const contest = await Contest.findById(contestID);
+        if (!contest){
+            return res.status(404).json({   
+                status: "Contest not found"
+            })
+        }
+        const message = {
+            from: 'TeamVanilla <teamvanilladip@gmail.com>',
+            to: receiver.email,
+            subject: "Contest Winner",
+            html: `<h1>You have been selected a winner in the '${contest.title}'</h1>`
+        }
+        transporter.sendMail(message, (err, info) => {
+            if (err) {
+                return res.status(500).json({status: "error",
+                    err
+                })
+            } else {
+                return res.status(200).json({
+                    status: 'Email Sent Successfully',
+                    info
+                })
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({ error });
+    }
 });
